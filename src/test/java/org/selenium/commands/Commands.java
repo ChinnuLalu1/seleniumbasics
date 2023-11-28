cd11b068cf2c46020ba99ca57a3beecdb871c80a
chinnuLalu123 <chinnu.obsqura@gmail.com>
2023-11-28 17:52:00 +0530

    xpath and css selector added.

--- src/test/java/org/selenium/commands/Commands.java
+++ src/test/java/org/selenium/commands/Commands.java
@@ -38,35 +38,82 @@ public class Commands extends Base {
         String expectedUserEmailId = "chinnu.obsqura@gmail.com";
         Assert.assertEquals(actualUserEmailId, expectedUserEmailId, "Invalid User ");
     }
+
     @Test
-    public void verifyDemoToursRegistration(){
+    public void verifyDemoToursRegistration() {
         driver.get("https://demo.guru99.com/test/newtours/register.php");
-        WebElement firstName= driver.findElement(By.name("firstName"));
+        WebElement firstName = driver.findElement(By.name("firstName"));
         firstName.sendKeys("Chinnu");
-        WebElement lastName= driver.findElement(By.name("lastName"));
+        WebElement lastName = driver.findElement(By.name("lastName"));
         lastName.sendKeys("lalu");
-        WebElement phnNumber=driver.findElement(By.name("phone"));
+        WebElement phnNumber = driver.findElement(By.name("phone"));
         phnNumber.sendKeys("7510829330");
-        WebElement email=driver.findElement(By.id("userName"));
+        WebElement email = driver.findElement(By.id("userName"));
         email.sendKeys("chinnu.obsqura@gmail.com");
-        WebElement address=driver.findElement(By.name("address1"));
+        WebElement address = driver.findElement(By.name("address1"));
         address.sendKeys("Kuttikattil");
-        WebElement city=driver.findElement(By.name("city"));
+        WebElement city = driver.findElement(By.name("city"));
         city.sendKeys("Kottayam");
-        WebElement state=driver.findElement(By.name("state"));
+        WebElement state = driver.findElement(By.name("state"));
         state.sendKeys("Kerala");
-        WebElement postelCode=driver.findElement(By.name("postalCode"));
+        WebElement postelCode = driver.findElement(By.name("postalCode"));
         postelCode.sendKeys("686632");
-        WebElement userName=driver.findElement(By.id("email"));
+        WebElement userName = driver.findElement(By.id("email"));
         userName.sendKeys("chinnu.obsqura@gmail.com");
-        WebElement password=driver.findElement(By.name("password"));
+        WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("chinnu123");
-        WebElement confirmPass=driver.findElement(By.name("confirmPassword"));
+        WebElement confirmPass = driver.findElement(By.name("confirmPassword"));
         confirmPass.sendKeys("chinnu123");
-        WebElement submitButton=driver.findElement(By.name("submit"));
+        WebElement submitButton = driver.findElement(By.name("submit"));
         submitButton.click();
     }
 
+    @Test
+    public void verifyLinkText() {
+        driver.get("https://demo.guru99.com/test/newtours/");
+        WebElement yourdestination = driver.findElement(By.linkText("your destination"));
+        yourdestination.click();
+    }
 
+    @Test
+    public void verifyPartialLinkText() {
+        driver.get("https://demo.guru99.com/test/newtours/");
+        WebElement destination = driver.findElement(By.partialLinkText("destination"));
+        destination.click();
+    }
 
+    @Test
+    public void validateDemoWebshopUserLogin() {
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement login = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[2]/a"));
+        login.click();
+        WebElement email = driver.findElement(By.xpath("//*[@id=\"Email\"]"));
+        email.sendKeys("chinnu.obsqura@gmail.com");
+        WebElement password = driver.findElement(By.xpath("//*[@id=\"Password\"]"));
+        password.sendKeys("chinnu123");
+        WebElement loginButton = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input"));
+        loginButton.click();
+        WebElement actualEmailElement = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a"));
+        String actualEmail = actualEmailElement.getText();
+        String expectedEmail = "chinnu.obsqura@gmail.com";
+        Assert.assertEquals(actualEmail, expectedEmail, "Invalid Email");
+    }
+
+    @Test
+    public void vrifyWebShopUsrLogin() {
+        driver.get("https://demowebshop.tricentis.com/");
+        WebElement login = driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.header > div.header-links-wrapper > div.header-links > ul > li:nth-child(2) > a"));
+        login.click();
+        WebElement email = driver.findElement(By.cssSelector("#Email"));
+        email.sendKeys("chinnu.obsqura@gmail.com");
+        WebElement password = driver.findElement(By.cssSelector("#Password"));
+        password.sendKeys("chinnu123");
+        WebElement loginButton = driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.master-wrapper-main > div.center-2 > div > div.page-body > div.customer-blocks > div.returning-wrapper > div.form-fields > form > div.buttons > input"));
+        loginButton.click();
+        WebElement actualEmailElement = driver.findElement(By.cssSelector("body > div.master-wrapper-page > div.master-wrapper-content > div.header > div.header-links-wrapper > div.header-links > ul > li:nth-child(1) > a"));
+        String actualEmail = actualEmailElement.getText();
+        String expectedEmail = "chinnu.obsqura@gmail.com";
+        Assert.assertEquals(actualEmail, expectedEmail, "Invalid Email");
+
+    }
 }
